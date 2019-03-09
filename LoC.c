19b2c60ae37d99eee660ebf3e0bc2f6659d2366e
jBernalQuintero <jbernal4713@gmail.com>
2019-03-09 09:35:35 -0800

    Created LoC file.  Contains main function.

---
+++ LoC.c
@@ -0,0 +1,23 @@
+//////////////////////////////////////////////////////////////
+//Name: Loc.c 												//
+//Author: Juan Bernal										//
+//Inputs: filename, operators								//
+//Description:												//
+//		This program is a line counter for code projects.	//
+//	This line counter opens the file and then counts the 	//
+//	number of lines of code in the file by counting what	//
+//	are considered 'legal' semicolons.  Legal semicolons 	//
+//	are semicolons outside of the context of strings or 	//
+//	or comments.  For example, ;;;;;; these should not be 	//
+//	counted.												//
+//															//
+//////////////////////////////////////////////////////////////
+
+int main(int argc, char **argv)
+{
+	//check for comand accuracy
+	if(argc < 2)
+	{
+		printf("Too few arguments.\n", %s);
+	}
+}
